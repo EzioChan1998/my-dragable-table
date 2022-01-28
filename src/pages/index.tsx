4915d40019b492cd85c5d467f89f545ed82dbb0e
EzioChan1998 <869909242@qq.com>
2022-01-28 14:31:02 +0800

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Table } from 'antd';
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Table></Table>
+    </div>
+  );
+}
